fe0b8282940c9030cda0ee24170c2247d010d348
Eroasulyan <eroasulyan2@gmail.com>
2024-12-19 10:50:40 +0400

    my new commit

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-let 
+let burger = document.querySelector("#burger");
 
+let menu = document.querySelector("#menu");
 
+burger.addEventListener ("click", () => {
 
+});  menu.classList.toggle("disp");
